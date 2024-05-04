5fa065f7fe9aa1fb4ce88b90224d2a0fb232df5d
Jhonatas J. Mendonça <118845977+Jhonatas56@users.noreply.github.com>
2024-05-04 11:28:58 -0300

    feat: add header component in layout and change default font to nunito sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lista de países",
@@ -15,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={nunitoSans.className}>
+        <div className="min-h-screen bg-gray-100">
+          <Header />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }

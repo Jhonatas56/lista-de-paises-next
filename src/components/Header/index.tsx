407e5d2d6a5478b0c6a61563855959f83d22a1b8
Jhonatas J. Mendonça <118845977+Jhonatas56@users.noreply.github.com>
2024-05-04 11:39:33 -0300

    refactor: redirect to home page when click in header content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import Logo from "../../../public/Logo.svg";
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
+  const router = useRouter();
+
+  const redirectToHome = () => {
+    router.push("/");
+  };
+
   return (
     <header className="flex items-center bg-white mx-auto max-w-[1920px] justify-center lg:justify-between py-4 lg:p-4">
-      <div className="flex gap-2 items-center text-gray-800 font-bold text-2xl ml-6">
+      <div
+        className="flex gap-2 items-center text-gray-800 font-bold text-2xl ml-6 cursor-pointer"
+        onClick={redirectToHome}
+      >
         <Image alt="logo" src={Logo} width={55} height={55} />
         <h1>Países do mundo</h1>
       </div>
